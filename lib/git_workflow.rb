d0c7ef76cb60048cff8afaa289996aed91853ba9
Matthew Denner <md12@sanger.ac.uk>
2010-07-02 10:59:00 +0100

    [fixes 4058394] Make the commands more verbose

--- lib/git_workflow.rb
+++ lib/git_workflow.rb
@@ -1,4 +1,5 @@
 require 'git_workflow/core_ext'
+require 'git_workflow/logging'
 require 'git_workflow/configuration'
 require 'git_workflow/story'
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,7 @@ require 'git_workflow'
 
 require 'shared_examples/configuration'
 require 'shared_examples/story'
+
+# Disable the logging output during tests
+GitWorkflow.logger = GitWorkflow::Story.logger = logger = Logger.new(STDOUT)
+logger.level = Logger::FATAL
